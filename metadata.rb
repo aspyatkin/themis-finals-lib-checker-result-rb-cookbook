72586d20d8e0c82569617c3dfc8f9eb8d23918c5
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-07-04 20:24:26 +0400

    Checkout from master

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'themis-finals-lib-checker-result-rb'
 description 'Installs and configures checker-result-rb'
-version '1.0.1'
+version '1.0.2'
 
 recipe 'themis-finals-lib-checker-result-rb', 'Installs and configures checker-result-rb'
 depends 'latest-git', '~> 1.1.6'
