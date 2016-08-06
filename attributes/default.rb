625356c50bd904251aa6badddda888b5959f6ff9
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-08-06 14:10:21 +0400

    Replace symbolized keys

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 id = 'themis-finals-lib-checker-result-rb'
 
-default[id][:basedir] = '/var/themis/finals/lib/checker-result-rb'
-default[id][:user] = 'vagrant'
-default[id][:group] = 'vagrant'
-default[id][:github_repository] = 'aspyatkin/themis-checker-result'
-default[id][:revision] = 'master'
+default[id]['basedir'] = '/var/themis/finals/lib/checker-result-rb'
+default[id]['user'] = 'vagrant'
+default[id]['group'] = 'vagrant'
+default[id]['github_repository'] = 'aspyatkin/themis-checker-result'
+default[id]['revision'] = 'master'
--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'themis-finals-lib-checker-result-rb'
 description 'Installs and configures checker-result-rb'
-version '1.0.2'
+version '1.0.3'
 
 recipe 'themis-finals-lib-checker-result-rb', 'Installs and configures checker-result-rb'
 depends 'latest-git', '~> 1.1.6'
