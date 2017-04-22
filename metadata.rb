e1e99011fda2432aca5a03ce129a722690dcc042
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2017-04-22 17:23:23 +0400

    Update dependencies

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name 'themis-finals-lib-checker-result-rb'
 description 'Installs and configures checker-result-rb'
-version '1.1.0'
+version '1.1.1'
 
 recipe 'themis-finals-lib-checker-result-rb', 'Installs and configures checker-result-rb'
-depends 'latest-git', '~> 1.1.11'
+depends 'git', '~> 6.0.0'
 depends 'git2', '~> 1.0.0'
-depends 'ssh-private-keys', '~> 1.0.0'
+depends 'ssh-private-keys', '~> 2.0.0'
+depends 'ssh_known_hosts', '~> 5.1.0'
